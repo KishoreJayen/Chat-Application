3560d7a5bec18b21eb3ab9d8666f5043353d5237
Kishore <Kishorejayen2020@gmail.com>
2024-06-20 23:14:48 +0530

    Adding client and server in same repo

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,14 @@
+const {login, register, getAllUsers, setAvatar } = require("../controllers/userController");
+
+
+const router = require("express").Router();
+
+
+router.post("/login", login);
+router.post("/register", register);
+router.post("/setavatar/:id", setAvatar);
+router.get("/allusers/:id", getAllUsers)
+
+
+
+module.exports = router;
